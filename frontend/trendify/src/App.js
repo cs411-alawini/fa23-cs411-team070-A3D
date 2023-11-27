bf950e1a44ef4cd9b51f5d9172e3c613861a8782
diegotaveras <diegoat2@illinois.edu>
2023-11-27 16:19:50 -0600

    frontend dropdown and general outlines

--- frontend/trendify/src/App.js
+++ frontend/trendify/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import SearchBar from './SearchBar';
+import Drop from './DropdownMenu';
 
 function App() {
   return (
@@ -8,13 +9,30 @@ function App() {
     <div className='NavBar'>
       <a href="Trendify">Trendify</a>
     </div>
-
-    <div className='container'>
-
+    
+    <div className='container' style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gridGap: 10 }}>
+      <div className='table-card'> 
+        <h3>Most Favorited</h3>
+        <ol className='favorited-list'> 
+        <li>hey</li> 
+        <li>test</li>  
+        <li>here</li>     {/*get this asynchronously with useeffect, fetch, axios */}
+        <li>lol</li> 
+        
+        </ol>
+      </div>
       <div className='main-card'>
       <SearchBar></SearchBar>
+      
 
       </div>
+
+      <div className='dropdown-card'>
+        <Drop/>
+      
+      
+      </div>
+
 
         
 
---
+++ frontend/trendify/src/DropdownMenu.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+
+export default function Drop() {
+  return (
+    <select class="form-select" aria-label="Default select example">
+  <option selected>Open this select menu</option>
+  <option value="1">All</option>
+  <option value="2">Category</option>
+  <option value="3">Wqhatevre</option>
+</select>
+  );
+}

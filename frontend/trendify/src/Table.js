3526d4305059a7e8ce71c8ae67d32d6dc726722e
diegotaveras <diegoat2@illinois.edu>
2023-12-06 04:50:45 -0600

    improvements to grading system; table view enhanced

--- frontend/trendify/src/Table.js
+++ frontend/trendify/src/Table.js
@@ -18,8 +18,11 @@ function LikeButton() {
 
 
 export default function SearchResult(props) {
-  
+
+
+
   return (
+    <>
 
     <MDBTable align='middle'>
       <MDBTableHead style={{position: "sticky" }}>
@@ -65,9 +68,10 @@ export default function SearchResult(props) {
           </td>
           <td>{tag.Total_videos}</td>
           <td>
-          <MDBBtn color='green' rounded size='sm'>
+            
+          <MDBBtn color={tag.Rating === 'A' ? 'green' : tag.Rating === 'B' ? 'yellow' : tag.Rating === 'C' ? 'orange' : 'red'} rounded size='sm'>
               {tag.Rating}
-            </MDBBtn>
+          </MDBBtn>
             
           </td>
         </tr>
@@ -80,102 +84,11 @@ export default function SearchResult(props) {
   </MDBSpinner> : ""
   
 )}
-        {/* <tr>
-          <td>
-            <div className='d-flex align-items-center'>
-              <a href="TagRating">
-              </a>
-              <img
-                src='https://mdbootstrap.com/img/new/avatars/8.jpg'
-                alt=''
-                style={{ width: '45px', height: '45px' }}
-                className='rounded-circle'
-              />
-              <div className='ms-3'>
-                <p className='fw-bold mb-1'>John Doe</p>
-                <p className='text-muted mb-0'>john.doe@gmail.com</p>
-              </div>
-            </div>
-          </td>
-          <td>
-            <p className='fw-normal mb-1'>Software engineer</p>
-            <p className='text-muted mb-0'>IT department</p>
-          </td>
-          <td>
-            <MDBBadge color='success' pill>
-              Active
-            </MDBBadge>
-          </td>
-          <td>Senior</td>
-          <td>
-            <MDBBtn color='link' rounded size='sm'>
-              Edit
-            </MDBBtn>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <div className='d-flex align-items-center'>
-              <img
-                src='https://mdbootstrap.com/img/new/avatars/6.jpg'
-                alt=''
-                style={{ width: '45px', height: '45px' }}
-                className='rounded-circle'
-              />
-              <div className='ms-3'>
-                <p className='fw-bold mb-1'>Alex Ray</p>
-                <p className='text-muted mb-0'>alex.ray@gmail.com</p>
-              </div>
-            </div>
-          </td>
-          <td>
-            <p className='fw-normal mb-1'>Consultant</p>
-            <p className='text-muted mb-0'>Finance</p>
-          </td>
-          <td>
-            <MDBBadge color='primary' pill>
-              Onboarding
-            </MDBBadge>
-          </td>
-          <td>Junior</td>
-          <td>
-            <MDBBtn color='link' rounded size='sm'>
-              Edit
-            </MDBBtn>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <div className='d-flex align-items-center'>
-              <img
-                src='https://mdbootstrap.com/img/new/avatars/7.jpg'
-                alt=''
-                style={{ width: '45px', height: '45px' }}
-                className='rounded-circle'
-              />
-              <div className='ms-3'>
-                <p className='fw-bold mb-1'>Kate Hunington</p>
-                <p className='text-muted mb-0'>kate.hunington@gmail.com</p>
-              </div>
-            </div>
-          </td>
-          <td>
-            <p className='fw-normal mb-1'>Designer</p>
-            <p className='text-muted mb-0'>UI/UX</p>
-          </td>
-          <td>
-            <MDBBadge color='warning' pill>
-              Awaiting
-            </MDBBadge>
-          </td>
-          <td>Senior</td>
-          <td>
-            <MDBBtn color='link' rounded size='sm'>
-              Edit
-            </MDBBtn>
-          </td>
-        </tr> */}
+      
       </MDBTableBody>
     </MDBTable>
+    
+  {/* <Chart></Chart> */}
+  </>
   );
 }
